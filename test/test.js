f53219313d7ee6bc5494308e0c210c56b46dd4d4
akaashanky <akaashanky@gmail.com>
2013-10-02 23:43:23 +0530

    First commit. Project structure, basic test.

---
+++ test/test.js
@@ -0,0 +1,27 @@
+var should = require('chai').should(),
+    supertest = require('supertest'),
+    api = supertest('http://localhost:3000');
+
+describe('test', function () {
+ it('should add 1+1 correctly', function (done) {
+   var onePlusOne = 1 + 1;
+   onePlusOne.should.equal(2);
+   // must call done() so that mocha know that we are... done.
+   // Useful for async tests.
+   done();
+ });
+
+ it('should 2 in response sum from add/1/1', function(done){
+ 	api.get('/add/1/1')
+ 	.expect(200)
+ 	.end(function(err, res){
+ 		should.not.exist(err);
+ 		res.text.should.equal('2');
+ 		done();
+ 	});
+ });
+
+
+
+
+});
